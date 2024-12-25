36f340750d8a4cc91a4ebaecacc0fcc70ca118e8
nolasemon <54684966+nolasemon@users.noreply.github.com>
2024-12-25 23:13:35 +0100

    Refine series tests

--- tests/src/series_test.cpp
+++ tests/src/series_test.cpp
@@ -6,6 +6,20 @@ using namespace Luxora;
 TEST(TestSeries, CreationTyped) {
 	Series<int> series({18, 30, 92, 15, 20});
 
+	ASSERT_EQ(series.size(), 5);
+	ASSERT_EQ(series.type(), typeid(int));
+	ASSERT_EQ(series.type_size(), 4);
+
+	ASSERT_EQ(series.count(), 5);
+	ASSERT_EQ(series.mean(), 35);
+	ASSERT_EQ(series.median(), 20);
+
+	Series<int> series2(std::vector<int>{18, 30, 92, 15, 20});
+
+	ASSERT_EQ(series.size(), 5);
+	ASSERT_EQ(series.type(), typeid(int));
+	ASSERT_EQ(series.type_size(), 4);
+
 	ASSERT_EQ(series.count(), 5);
 	ASSERT_EQ(series.mean(), 35);
 	ASSERT_EQ(series.median(), 20);
@@ -13,6 +27,10 @@ TEST(TestSeries, CreationTyped) {
 
 TEST(TestSeries, WithNA) {
 	Series<int> series({18, 30, 92, {}, 20});
+
+	ASSERT_EQ(series.size(), 5);
+	ASSERT_EQ(series.type(), typeid(int));
+	ASSERT_EQ(series.type_size(), 4);
 
 	ASSERT_EQ(series.count(), 4);
 	ASSERT_EQ(series.mean(), 40);
@@ -38,4 +56,18 @@ TEST(TestSeries, TestFillNa) {
 	ASSERT_EQ(series2.count(), 5);
 	ASSERT_EQ(series2.mean(), 37);
 	ASSERT_EQ(series2.median(), 25);
+}
+
+TEST(TestSeries, TestIdentifyNa) {
+	Series<int> series(std::vector<int>{-1, 5, 4, 6, -1, 8});
+
+	series.identify_na(-1);
+
+	ASSERT_EQ(series.size(), 6);
+	ASSERT_EQ(series.count(), 4);
+
+	series.fill_na(42);
+
+	ASSERT_EQ(series.size(), 6);
+	ASSERT_EQ(series.count(), 6);
 }
